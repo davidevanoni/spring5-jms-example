bad96cff83e7113896f7ccf6667e38437ad5735d
Davide Vanoni <davide.vanoni@gmail.com>
2020-05-04 09:34:17 +0200

    Initial commit

---
+++ src/main/java/dv/springframework/spring5jmsexample/Spring5JmsExampleApplication.java
@@ -0,0 +1,13 @@
+package dv.springframework.spring5jmsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JmsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JmsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dv/springframework/spring5jmsexample/Spring5JmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dv.springframework.spring5jmsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JmsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
