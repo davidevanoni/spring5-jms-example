ba3496ad7564c11380b67acea2972f3e463563e0
Davide Vanoni <davide.vanoni@gmail.com>
2020-05-05 15:40:52 +0200

    Added an example of synchronous JSM queue (send and receive)

--- src/main/java/dv/springframework/spring5jmsexample/sender/HelloSender.java
+++ src/main/java/dv/springframework/spring5jmsexample/sender/HelloSender.java
@@ -1,12 +1,18 @@
 package dv.springframework.spring5jmsexample.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dv.springframework.spring5jmsexample.config.JmsConfig;
 import dv.springframework.spring5jmsexample.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -14,11 +20,10 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
-        System.out.println("I'm sending a message");
-
         HelloWorldMessage message = HelloWorldMessage
                 .builder()
                 .id(UUID.randomUUID())
@@ -27,6 +32,31 @@ public class HelloSender {
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 
-        System.out.println("Message sent!");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+        System.out.println("I'm sending a message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receivedMsg = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, session -> {
+            Message helloMessage;
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "dv.springframework.spring5jmsexample.model.HelloWorldMessage");
+                System.out.println("Sending hello!");
+                return helloMessage;
+            } catch (JsonProcessingException e) {
+                throw new JMSException("boom");
+            }
+
+        });
+
+        System.out.println(receivedMsg != null ? receivedMsg.getBody(String.class) : null);
     }
 }
